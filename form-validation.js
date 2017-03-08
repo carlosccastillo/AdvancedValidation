caad17626377cbc6bae8460c6ee4ef63f2dc132e
Unknown <ccastillo15@mail.csuchico.edu>
2017-03-07 22:46:55 -0800

    FIxed validation

--- form-validation.js
+++ form-validation.js
@@ -3,17 +3,28 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
 
-  //Show message that there is an error with the username...
+  //Confirms username is 6 characters or more
+  if (userEntered.length >= 6){
+    document.getElementById("usernameGroup").classList.add("has-success");
+  }
+  //Shows message that there is an error with the password
+  else {
   document.getElementById("usernameError").innerHTML="Bad username.";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
   document.getElementById("usernameGroup").classList.add("has-error");
+  }
 
   //Show message that there is an error with the password...
+  if (passEntered == "password"){
   document.getElementById("passwordError").innerHTML="Bad password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
   document.getElementById("passwordGroup").classList.add("has-error");
+  }
+
+  //Confirms password was valid; no error;
+  else {
+    document.getElementById("passwordGroup").classList.add("has-success");
+  }
 }
